5348ba709d649084e089d99bea00381e10b6f604
Evangelos5641 <evangelos7328@gmail.com>
2026-01-30 23:06:28 +0200

    db

--- lib/dbconnect.php
+++ lib/dbconnect.php
@@ -1,13 +1,18 @@
 <?php
-$host='';
-$db='';
+$host='localhost';
+$db='webgame';
 
 require_once "lib/db_upass.php";
 
 $user = $DB_USER;
 $pass = $DB_PASS;
 
-$mysqli = new mysqli($host,$user,$pass,$db);
+if(gethostname()=='users.iee.ihu.gr'){
+    $mysqli = new mysqli($host,$user,$pass,$db,null,'');
+}
+else{
+    $mysqli = new mysqli($host,$user,$pass,$db);
+}
 
 if($mysqli->connect_errno){
     echo 'Failed to connect to MySQL';
